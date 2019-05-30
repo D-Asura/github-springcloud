7147ce83233eda7c99e1ea5124d4156d72e2940e
Dragon <1796620318@qq.com>
2019-05-30 14:03:43 +0800

    初始化springboot

---
+++ src/main/java/com/dragon/githubspringcloud/GithubSpringcloudApplication.java
@@ -0,0 +1,13 @@
+package com.dragon.githubspringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubSpringcloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubSpringcloudApplication.class, args);
+    }
+
+}
