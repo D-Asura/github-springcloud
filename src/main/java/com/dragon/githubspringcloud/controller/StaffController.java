550826c504c94512e4af21287053dea6434c84b9
Dragon <1796620318@qq.com>
2019-07-01 17:26:04 +0800

    .0.0.0.1

--- src/main/java/com/dragon/githubspringcloud/controller/StaffController.java
+++ src/main/java/com/dragon/githubspringcloud/controller/StaffController.java
@@ -5,10 +5,12 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.api.ApiController;
 import com.dragon.githubspringcloud.entity.Staff;
 import com.dragon.githubspringcloud.service.IStaffService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,8 +31,17 @@ public class StaffController extends ApiController {
     @ApiOperation("员工查询")
     @GetMapping("/all")
     public List<Staff> list() {
-
         return staffService.list();
+    }
+
+    @ApiOperation("员工查询")
+    @GetMapping("/{staff}/{ssss}")
+    public boolean save(@PathVariable String ssss,@PathVariable String staff) {
+        Staff s = new Staff();
+        s.setPhoneNum("dasd");
+        logger.info(staff);
+        logger.info(ssss);
+        return false;
     }
 
 }
